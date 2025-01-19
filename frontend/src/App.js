6db4ee0b859bf79f619e9242bbf8ffa32002fe1c
himav95 <himaviamarakoon@gmail.com>
2025-01-19 23:53:04 +0530

    modified page navigation in App.js, updated Header.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,27 +3,44 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import Services from "./Pages/Services";
+import Products from "./Pages/Products";
+import RequestOnline from "./Pages/RequestOnline";
 import Contact from "./Pages/Contact";
 import { Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
+     <Navbar bg="light" expand="lg" className="" id="navbarMain"> 
+      <Navbar.Brand>Hela Athkam</Navbar.Brand>
+      <Container>
+        {/* First row content */}
+        {/* Second row content */}
+      </Container>
+      </Navbar>
+
     <BrowserRouter>
-    <Header />
+  
+    <Header /> 
     
+
     <Container className="container-fluid vh-100" style={{marginTop:20}}>
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/requestonline" element={<RequestOnline />} />
       </Routes>
 
     </Container>
     <Footer />
     </BrowserRouter>
-    
+
     </>
   );
 }
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,19 +1,30 @@
 import { Container, Nav, Navbar } from 'react-bootstrap' ;
+
 function Header () {
     return(
         
-        
+
         <Navbar variant='dark' sticky='top' bg='primary'>
             <Container>
-                <Navbar.Brand href='/'>Hela Athkam</Navbar.Brand>
-                <Nav variant='pills'>
-                    <Nav.Link href='/'>Home</Nav.Link>
-                    <Nav.Link href='/about'>About</Nav.Link>
-                    <Nav.Link href='/contact'>Contact</Nav.Link>
+            
+
+                
+                
+                <Nav className='d-flex justify-content-center' variant='pills'>
+                    <Nav.Link className='me-5' href='/'>Home</Nav.Link>
+                    <Nav.Link className='mx-5' href='/about'>About</Nav.Link>
+                    <Nav.Link className='mx-5' href='/contact'>Contact</Nav.Link>
+                    <Nav.Link className='mx-5' href='/services'>Services</Nav.Link>
+                    <Nav.Link className='mx-5' href='/products'>Products</Nav.Link>
+                    <Nav.Link className='mx-5' href='/requestonline'>Request Online</Nav.Link>
+                    
                 </Nav>
+
             </Container>
         </Navbar>
+
         
     )
 }
-export default Header
+
+export default Header;
