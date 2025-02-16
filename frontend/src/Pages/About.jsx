624b147a9063bd893eff7e913824f305d205fb72
himav95 <himaviamarakoon@gmail.com>
2025-02-16 20:24:49 +0530

    modified src/Pages/Home.jsx, OrderOnline.jsx and About.jsx

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -1,9 +1,103 @@
-function About () {
-    return (
-        <div>
-            <h1>About</h1>
-            <p>This is About Page</p>
-        </div>
-    )
+import { Card, Row, Col} from 'react-bootstrap';
+
+// import Hela athkam: Page css file here.
+import '../Asset/Style/Helaathkam_Page.css';
+
+function About() {
+  return (
+    <>
+      {/* about page headings */}
+      <Row className="mb-4">
+        <Col></Col>
+        <Col xs={10}>
+          <h2 className="mb-2">
+            Echoes of Tradition, Crafted for Tomorrow: Hela Athkam
+          </h2>
+          <h4 className="mb-5">
+            Where Heritage Transcends Time, Passed Down for Generations to
+            Cherish..
+          </h4>
+        </Col>
+        <Col></Col>
+      </Row>
+
+      {/* about page content para */}
+      <Row className="componentSpace">
+        <Col></Col>
+        <Col xs={10}>
+          <Card id="aboutContentCard">
+            <Row className="mb-3">
+              <Col>
+                <p>
+                  Step into a world where artistry meets tradition, and
+                  craftsmanship tells a story. Hela Athkam is not just a
+                  handicraft shop; it's a celebration of heritage, creativity,
+                  and the skilled hands that bring our exquisite pieces to life.
+                </p>
+
+                <p>
+                  Discover a curated collection of handcrafted wonders that
+                  reflect the traditional touch of Sri lankan culture. Each
+                  piece is a testament to the passion and dedication of our
+                  artisans, who pour their hearts into every creation. From
+                  traditionally designed houseware, tableware to meticulously
+                  designed bags, our shop is a treasure trove of unique and
+                  timeless handicrafts.
+                </p>
+              </Col>
+            </Row>
+
+            <Row className="mb-3">
+              <Col>
+                <p>
+                  At Hela Athkam, we believe in the beauty of preserving age-old
+                  techniques while embracing contemporary aesthetics. Our
+                  mission is to connect you with the soulful essence of
+                  craftsmanship, offering you not just products, but stories
+                  woven with threads of tradition. Whether you are looking for a
+                  special gift, a statement piece for your home, or simply want
+                  to indulge in the joy of exploring exquisite craftsmanship,
+                  Hela Athkam welcomes you with open arms. Join us on a journey
+                  where every purchase supports the livelihoods of skilled
+                  artisans and helps keep the flame of Sri Lankan traditional
+                  craftsmanship alive.
+                </p>
+              </Col>
+            </Row>
+
+            <Row className="mb-3">
+              <Col>
+                <p>
+                  In the realm of traditional handicrafts, value extends far
+                  beyond the tangible. Each piece crafted by skilled artisans
+                  not only embodies cultural pride and heritage but also
+                  contributes to environmental health. By choosing traditional
+                  handicrafts, you are making a conscious decision to support
+                  sustainable practices and reduce the environmental impact
+                  associated with mass production. These timeless creations are
+                  a testament to the enduring craftsmanship passed down through
+                  generations, adding intrinsic value to your possessions. With
+                  every purchase, you become a steward of tradition, fostering a
+                  connection to cultural roots while promoting ethical and
+                  eco-friendly choices. Embrace the richness of tradition,
+                  uphold your cultural pride, and nurture environmental
+                  well-being : all through these handicrafts, which toned and
+                  woven with passion.
+                </p>
+                <p>
+                  Thank you for choosing Hela Athkam : where every creation is a
+                  masterpiece, and every purchase is a contribution to the
+                  legacy of handmade excellence. Explore, experience, and
+                  embrace the art of Hela Athkam.
+                </p>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+        <Col></Col>
+      </Row>
+    </>
+  );
 }
-export default About
+
+export default About;
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,20 +1,114 @@
-import { Card, Button } from 'react-bootstrap' ;
-function Home () {
-    return (
+import Carousel from 'react-bootstrap/Carousel';
+import { Row, Col } from 'react-bootstrap';
+import Image14 from '../Asset/Images/Image14.jpg';
+import Image17 from '../Asset/Images/Image17.jpg';
+import Image16 from '../Asset/Images/Image16.jpg';
+import Image18 from '../Asset/Images/Image18.jpg';
+import Image15 from '../Asset/Images/Image15.jpg';
+import Image22 from '../Asset/Images/Image22.jpg';
+
+// import Hela athkam: Page css file here.
+import '../Asset/Style/Helaathkam_Page.css';
+// import Hela athkam: Form css file here.
+import '../Asset/Style/Helaathkam_Form.css';
+
+function Home() {
+  return (
+    <div>
+      <div className="backgroundImageContainer">
+        {/* Home page background image content is here. */}
+
+        {/* Mini image block content is here. */}
         <div>
-            <h1>Home</h1>
-            <p>This is the Home Page.</p>
-            <Card>
-                <Card.Header>Featured</Card.Header>
-                <Card.Body>
-                    <Card.Title>Special Title Treatment</Card.Title>
-                    <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
-                    </Card.Text>
-                    <Button variant='Primary'>Go Somewhere</Button>
-                </Card.Body>
-            </Card>
+          <Row className="rowMargin">
+            <Col></Col>
+            <Col>
+              <h6>Houseware</h6>
+              <div>
+                <img className="imageFirst" src={`${Image16}`} alt="Image16" />
+              </div>
+            </Col>
+
+            <Col>
+              <h6>Kitchenware</h6>
+              <div>
+                <img className="imageFirst" src={`${Image14}`} alt="Image14" />
+              </div>
+            </Col>
+
+            <Col>
+              <h6>Tableware</h6>
+              <div>
+                <img className="imageFirst" src={`${Image18}`} alt="Image18" />
+              </div>
+            </Col>
+
+            <Col>
+              <h6>Accessories</h6>
+              <div>
+                <img className="imageFirst" src={`${Image17}`} alt="Image17" />
+              </div>
+            </Col>
+            <Col></Col>
+          </Row>
+
+          {/* Home page welcome paragraph content is here. */}
+          <Row className="rowMargin">
+            <Col></Col>
+            <Col className="col-lg-6">
+              <h5 style={{ fontFamily: 'Arial sans-serif' }}>
+                Welcome to Hela Athkam, where craftsmanship meets curated
+                elegance! Immerse yourself in a world of handcrafted wonders
+                that blend tradition with contemporary allure. Our pieces are
+                more than just products—they are a celebration of heritage and a
+                testament to the skilled artisans behind each creation.
+              </h5>
+            </Col>
+            <Col></Col>
+          </Row>
+
+          {/* Carousel block and its side paragraph content is here. */}
+          <Row className="rowMargin">
+            <Col></Col>
+
+            {/* Carousel block content. */}
+            <Col className="">
+              <Carousel id="carouselBlock" fade>
+                <Carousel.Item interval={4500}>
+                  <img
+                    className="carousellg"
+                    src={`${Image22}`}
+                    alt="Image22"
+                  />
+                  <Carousel.Caption></Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item interval={4500}>
+                  <img
+                    className="carousellg"
+                    src={`${Image15}`}
+                    alt="Image15"
+                  />
+                  <Carousel.Caption></Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+            </Col>
+
+            {/* carousel block side paragraph content. */}
+            <Col className="">
+              <h5>
+                Diverse range of handicrafted essentials, Elegant decoratives to
+                adorn your home or work place, Practical essentials enhance
+                functionality, or thoughtful gifts to loved ones. Explore and
+                discover what matches your taste..
+              </h5>
+            </Col>
+            <Col></Col>
+          </Row>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default Home
+
+export default Home;
