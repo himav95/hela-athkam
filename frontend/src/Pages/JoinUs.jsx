8e236471ecdd3592c193118fe190c20443fb75ce
himav95 <himaviamarakoon@gmail.com>
2025-02-26 21:59:50 +0530

    changes on src/PagesJoinUs.jsx

--- frontend/src/Pages/JoinUs.jsx
+++ frontend/src/Pages/JoinUs.jsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import { Row, Col, Button } from 'react-bootstrap';
 import CraftsmanRequest from '../Forms/CraftsmanRequest';
-import { useState } from 'react';
+
 
 function JoinUs() {
   // Craftsmen request form, state and function details.
